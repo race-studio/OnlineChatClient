16ffea2512bfd33301b802f81baa58c746c6eff7
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 17:59:24 +0300

    Подготвил проект к работе

--- OnlineChatClient/ClientProgram.cs
+++ OnlineChatClient/ClientProgram.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;      
+using System.Net.Sockets;
 
 namespace OnlineChatClient
 {
-    class Program
+    class ClientProgram
     {
         static void Main(string[] args)
         {
