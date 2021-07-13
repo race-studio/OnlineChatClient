36bc4d24b8f66553dce9ffc811768b78274c46e8
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 18:39:34 +0300

    Формирование и отправка запроса серверу

--- OnlineChatClient/ClientProgram.cs
+++ OnlineChatClient/ClientProgram.cs
@@ -11,7 +11,38 @@ namespace OnlineChatClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int    port = 8005;
+            string ip = "127.0.0.1";
+            string message = "a";
+
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Socket Ready");
+
+            socket.Connect( ipPoint );
+            
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            socket.Send(data);
+
+            data = new byte[256]; // буфер для ответа
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0; // количество полученных байт
+
+            do
+            {
+                bytes = socket.Receive(data, data.Length, 0);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (socket.Available > 0);
+            Console.WriteLine("ответ сервера: " + builder.ToString());
+
+            // закрываем сокет
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            Console.WriteLine("end!");
+            string m = Console.ReadLine();
         }
     }
 }
